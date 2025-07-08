e5e3e289158bc8b3d2a92742e21704213c12dcb4
Maksick <pitalenkomaksim@gmail.com>
2025-07-08 16:56:03 +0300

    add boss

--- Cill Guy Defffence.py
+++ Cill Guy Defffence.py
@@ -14,6 +14,8 @@ boom = pygame.image.load("boom.png")
 boom = pygame.transform.scale(boom,(200,200))
 boom_timer = 0
 
+bosses = []
+boss_spaun_timer = 0
 
 enemies = []
 enemy_spawn_timer = 0
@@ -36,20 +38,24 @@ class Boss:
         self.boss = pygame.transform.scale(self.boss,(250,150))
         self.boss_x = 360
         self.boss_y = 0
-        self.boss_health = 20
-        self.boss_speed = 0.5
+        self.side = random.randint(0,3)
+        self.boss_health = 200
+        self.boss_speed = 1
     def apear(self):
-         screen.blit(self.boss, (self.boss_x, self.boss_y))
+        screen.blit(self.boss, (self.boss_x, self.boss_y))
     def take_damage(self):
-         self.boss_y -= 10
-         self.boss_health -= player.player_attack
+        self.boss_y -= 1.3
+        self.boss_health -= player.player_attack
     def move(self):
-         self.boss_y += self.boss_speed 
+        self.boss_y += self.boss_speed 
+    def boss_die(self):
+        bosses.remove(boss)
+
               
 
 
 
-class EnemyLvL1(Tower):
+class Enemies(Tower):
     def __init__(self):
         enemy_images = [
             "crok.png",
@@ -108,7 +114,7 @@ class EnemyLvL1(Tower):
         enemy_rect = pygame.Rect(self.enemy1_x, self.enemy1_y, 50, 50)
         return enemy_rect.colliderect(pygame.Rect(400, 300, 200, 200))
     
-class Player(EnemyLvL1):
+class Player(Enemies):
     def __init__(self):
       self.player_image = pygame.image.load("image.png")  
       self.player_image = pygame.transform.scale(self.player_image, (100, 100))
@@ -120,19 +126,25 @@ class Player(EnemyLvL1):
       self.player_score = 0
     def move(self):
          keys = pygame.key.get_pressed()
-         if keys[pygame.K_LEFT] and self.player_x > 0:
+         if keys[pygame.K_a] and self.player_x > 0:
               self.player_x -= self.player_speed
-         if keys[pygame.K_RIGHT] and self.player_x < screen_x - self.player_size:
+         if keys[pygame.K_d] and self.player_x < screen_x - self.player_size:
               self.player_x += self.player_speed
-         if keys[pygame.K_UP] and self.player_y > 0:
+         if keys[pygame.K_w] and self.player_y > 0:
               self.player_y -= self.player_speed
-         if keys[pygame.K_DOWN] and self.player_y < screen_y - self.player_size:
+         if keys[pygame.K_s] and self.player_y < screen_y - self.player_size:
               self.player_y += self.player_speed
     def draw(self):
          screen.blit(self.player_image, (self.player_x, self.player_y))
     def punch(self):
         enemy_rect = pygame.Rect(enemy.enemy1_x, enemy.enemy1_y, 50, 50)
         return enemy_rect.colliderect(pygame.Rect(self.player_x,self.player_y,100,100)) and enemy.health != 0
+    def punch_boss(self):
+        boss_rect = pygame.Rect(boss.boss_x, boss.boss_y, 250, 150)
+        return boss_rect.colliderect(pygame.Rect(self.player_x,self.player_y,100,100)) and boss.boss_health != 0
+    def kill_boss(self):
+        boss_rect = pygame.Rect(boss.boss_x, boss.boss_y, 250, 150)
+        return boss_rect.colliderect(pygame.Rect(self.player_x,self.player_y,100,100)) and boss.boss_health == 0
     def kill(self):
         enemy_rect = pygame.Rect(enemy.enemy1_x, enemy.enemy1_y, 50, 50)
         return enemy_rect.colliderect(pygame.Rect(self.player_x,self.player_y,100,100)) and enemy.health == 0
@@ -140,7 +152,6 @@ class Player(EnemyLvL1):
 
 player = Player()
 tower = Tower()
-boss = Boss()
 
 while running:
     for event in pygame.event.get():
@@ -155,13 +166,8 @@ while running:
 
     enemy_spawn_timer += 1
     if enemy_spawn_timer >= 90:
-        enemies.append(EnemyLvL1())
-        enemy_spawn_timer = 0
-
-    if player.player_score >= 1:
-        boss.apear()
-        boss.move()
-
+        enemies.append(Enemies())
+        enemy_spawn_timer = 0    
     for enemy in enemies[:]:
 
         enemy.move()
@@ -179,6 +185,19 @@ while running:
             screen.blit(boom, (400, 300))
             tower.tower_health -= 1
             enemies.remove(enemy)
+
+    boss_spaun_timer +=1
+    if boss_spaun_timer == 600:
+        boss_spaun_timer = 0
+        bosses.append(Boss())
+
+    for boss in bosses[:]:
+        boss.move()
+        boss.apear()
+        if player.punch_boss():
+             boss.take_damage()
+        elif player.kill_boss():
+             bosses.remove(boss)
 
 
     font = pygame.font.SysFont(None, 36)
